6270b446060845b8900549c54711ba830dc056be
Idan Tomer <idan.tomer2@gmail.com>
2020-01-17 16:03:18 +0200

    add all DTO files
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

---
+++ Product.py
@@ -0,0 +1,6 @@
+class Product:
+    def __init__(self, id, description, price, quantity):
+        self.id = id
+        self.description = description
+        self.price = price
+        self.quantity = quantity
---
+++ Supplier.py
@@ -0,0 +1,5 @@
+class Supplier:
+    def __init__(self, id, name, contact_information):
+        self.id = id
+        self.name = name
+        self.contact_information = contact_information
