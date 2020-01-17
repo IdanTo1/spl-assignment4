abc6671022cbc584fa81bc57fe4399c39407593b
Idan Tomer <idan.tomer2@gmail.com>
2020-01-17 18:50:24 +0200

    implement initiate.py
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

---
+++ initiate.py
@@ -0,0 +1,70 @@
+import sqlite3
+import sys
+
+from DAO import _Employees, _Suppliers, _Products, _CoffeeStands, _Activities
+from DTO import *
+
+DB_FILE_NAME = "moncafe.db"
+
+
+def create_tables(cursor):
+    cursor.execute("""CREATE TABLE Employees(id INTEGER PRIMARY KEY,
+                                                name TEXT NOT NULL,
+                                                salary REAL NOT NULL,
+                                                coffee_stand INTEGER REFERENCES Coffee_stands(id))
+    
+    """)
+    cursor.execute("""CREATE TABLE Suppliers(id INTEGER PRIMARY KEY,
+                                                name TEXT NOT NULL,
+                                                contact_information TEXT)
+    """)
+    cursor.execute("""CREATE TABLE Products(id INTEGER PRIMARY KEY,
+                                                description TEXT NOT NULL,
+                                                price REAL NOT NULL,
+                                                quantity INTEGER NOT NULL)
+    """)
+    cursor.execute("""CREATE TABLE Coffee_stands(id INTEGER PRIMARY KEY,
+                                                    location TEXT NOT NULL,
+                                                    number_of_employees INTEGER)
+    """)
+    cursor.execute("""CREATE TABLE Activities(product_id INTEGER REFERENCES Products(id),
+                                                quantity INTEGER NOT NULL,
+                                                activator_id INTEGER NOT NULL,
+                                                date DATE NOT NULL)
+    """)
+
+
+def fill_tables(cursor, config_file):
+    employees = _Employees(cursor)
+    suppliers = _Suppliers(cursor)
+    products = _Products(cursor)
+    coffee_stands = _CoffeeStands(cursor)
+    activities = _Activities(cursor)
+    with open(config_file, "r") as f:
+        for line in f.readlines():
+            args = line.split(',')
+            type = args.pop(0).strip()
+            if type == "E":
+                employee = Employee(*args)
+                employees.insert(employee)
+            elif type == "S":
+                supplier = Supplier(*args)
+                suppliers.insert(supplier)
+            else:  # type == "C", valid file assuemed
+                coffee_stand = CoffeeStand(*args)
+                coffee_stands.insert(coffee_stand)
+
+
+def main(config_file):
+    open(DB_FILE_NAME, 'w').close()  # clear the db file
+
+    db_conn = sqlite3.connect(DB_FILE_NAME)
+    db_cursor = db_conn.cursor()
+    create_tables(db_cursor)
+    fill_tables(db_cursor, config_file)
+
+    db_conn.commit()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
