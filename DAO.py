4f7d9bd1810913f277cb601fa3b038388f2ddaac
Idan Tomer <idan.tomer2@gmail.com>
2020-01-18 16:58:00 +0200

    move complicated selects to printdb.py and fix implementation
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- DAO.py
+++ DAO.py
@@ -14,20 +14,6 @@ class _Employees:
         for one in all_data:
             print(one)
 
-    def print_report(self):
-        print("Employees report")
-        self._cursor.execute("""SELECT emp.name, emp.salary, cs.location, sum(prod.price * act.quantity) 
-        FROM ((Employees AS emp
-        INNER JOIN Coffee_stands AS cs 
-        ON emp.coffee_stand = cs.id)
-        INNER JOIN Activities AS act on emp.id = act.activator_id) 
-        INNER JOIN Products as prod on act.product_id = prod.id
-        ORDER BY emp.name ASC
-        """)
-        all_data = self._cursor.fetchall()
-        for one in all_data:
-            print(*one)
-
 
 class _Suppliers:
     def __init__(self, cursor):
@@ -92,22 +78,6 @@ class _Activities:
     def print_table(self):
         print("Activities")
         self._cursor.execute("SELECT * FROM Activities ORDER BY Activities.date ASC")
-        all_data = self._cursor.fetchall()
-        for one in all_data:
-            print(one)
-
-    def print_report(self):
-        print("Activities")
-        self._cursor.execute("""SELECT act.date, prod.description, act.quantity, emp.name, supp.name 
-        FROM ((Activities AS act 
-        INNER JOIN Products AS prod
-        ON act.product_id = prod.id)
-        LEFT OUTER JOIN Employees AS emp
-        ON act.activator_id = emp.id)
-        LEFT OUTER JOIN Suppliers AS supp
-        ON act.activator_id = supp.id
-        ORDER BY act.date DESC 
-        """)
         all_data = self._cursor.fetchall()
         for one in all_data:
             print(one)
--- printdb.py
+++ printdb.py
@@ -5,21 +5,73 @@ from DAO import _Employees, _Suppliers, _Products, _CoffeeStands, _Activities
 from DTO import *
 
 
+def print_employees_report(cursor):
+    print("Employees report")
+    cursor.execute("""SELECT emp.name, emp.salary, cs.location, sum(prod.price * act.quantity)
+    FROM ((Employees AS emp
+    INNER JOIN Coffee_stands AS cs 
+    ON emp.coffee_stand = cs.id)
+    LEFT OUTER JOIN (Activities AS act
+    INNER JOIN Products AS prod ON act.product_id = prod.id)
+    ON act.activator_id = emp.id)
+    GROUP BY emp.id
+    """)
+
+    all_data = cursor.fetchall()
+    for one in all_data:
+        print(*one)
+
+
+# , sum(prod.price * act.quantity)
+# LEFT OUTER JOIN (Activities AS act
+#     INNER JOIN Products as prod on act.product_id = prod.id)
+#     ON emp.id = act.activator_id)
+#   ON emp.id = act.activator_id
+#   ORDER BY emp.name ASC
+
+
+def print_activities_report(cursor):
+    print("Activities")
+    cursor.execute("""SELECT act.date, prod.description, act.quantity, emp.name, supp.name 
+    FROM ((Activities AS act 
+    INNER JOIN Products AS prod
+    ON act.product_id = prod.id)
+    LEFT OUTER JOIN Employees AS emp
+    ON act.activator_id = emp.id)
+    LEFT OUTER JOIN Suppliers AS supp
+    ON act.activator_id = supp.id
+    ORDER BY act.date ASC
+    """)
+    all_data = cursor.fetchall()
+    for one in all_data:
+        print(one)
+
+
 def main():
     db_exists = os.path.isfile(DB_FILE_NAME)
     if not db_exists:
         raise FileNotFoundError(("data base %s does not exists" % DB_FILE_NAME))
     db_conn = sqlite3.connect(DB_FILE_NAME)
     db_cursor = db_conn.cursor()
+
+    db_cursor.execute("""INSERT INTO Activities (product_id, quantity, activator_id, date) VALUES (?, ?, ?, ?)
+                """, [9004, 100, 102, 20001010])
+    db_cursor.execute("""INSERT INTO Activities (product_id, quantity, activator_id, date) VALUES (?, ?, ?, ?)
+        """, [9002, 10, 1004, 20001010])
+    db_cursor.execute("""INSERT INTO Activities (product_id, quantity, activator_id, date) VALUES (?, ?, ?, ?)
+            """, [9002, 30, 1004, 20001011])
+    db_cursor.execute("""INSERT INTO Activities (product_id, quantity, activator_id, date) VALUES (?, ?, ?, ?)
+            """, [9004, 20, 1003, 20001010])
+
     _Activities(db_cursor).print_table()
     _CoffeeStands(db_cursor).print_table()
     _Employees(db_cursor).print_table()
     _Products(db_cursor).print_table()
     _Suppliers(db_cursor).print_table()
     print()
-    _Employees(db_cursor).print_report()
+    print_employees_report(db_cursor)
     print()
-    _Activities(db_cursor).print_report()
+    print_activities_report(db_cursor)
 
 
 if __name__ == "__main__":
